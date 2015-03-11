e3a6f00fbbe700a660a3575f185ba3d833f92d0d
Ben Lavender <bhuga@github.com>
2015-03-10 19:13:38 -0500

    Dat try

--- lib/setup.rb
+++ lib/setup.rb
@@ -1,10 +1,12 @@
 require "bundler/setup"
 Bundler.require(:default)
 Dotenv.load
-#require "active_support/dependencies"
 require "active_support/inflector"
+require "active_support/core_ext/object/try"
+
 lib = File.expand_path("../", __FILE__)
 puts "adding #{lib} to load path"
 $:.unshift lib
 require "connection"
-#ActiveSupport::Dependencies.autoload_paths = [lib]
+
+require "io/console"
